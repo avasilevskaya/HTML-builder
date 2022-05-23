eb95aaeb3c3031bf29fb717514cb585f3857ec82
avasilevskaya <anst.vasilevskaya@gmail.com>
2022-05-23 22:01:22 +0300

    refactor: add err catch; minor changes

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,20 +1,24 @@
-const fsPromises = require('fs/promises');
+const { readdir, stat } = require('fs/promises');
 const path = require('path');
 
 (async () => {
-  const fileNamesArr = await fsPromises.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
-
-  for (let item of fileNamesArr) {
-    const fileNameWithExt = item.name;
-    const pathToFile = path.join(__dirname, 'secret-folder', fileNameWithExt);
-    const pathParsed = path.parse(pathToFile);
-    const stat = await fsPromises.stat(pathToFile);
-
-    let ext = pathParsed.ext.slice(1);
-    if (ext ==='') ext = 'NA';
-
-    if (stat.isFile()) { 
-      console.log(`${pathParsed.name} - ${ext} - ${stat.size}b`);
-    } 
+  try {
+    const arrFiles = await readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
+    
+    for (let item of arrFiles) {
+      const fileNameWithExt = item.name;
+      const pathToFile = path.join(__dirname, 'secret-folder', fileNameWithExt);
+      const pathParsed = path.parse(pathToFile);
+      const stats = await stat(pathToFile);
+  
+      let ext = pathParsed.ext.slice(1);
+      if (ext ==='') ext = 'NA';
+  
+      if (stats.isFile()) { 
+        console.log(`${pathParsed.name} - ${ext} - ${stats.size}b`);
+      } 
+    }
+  } catch (err) {
+    console.log(`Ошибка: ${err.message}`);
   }
 })();
