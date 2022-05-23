bfa78bfbdd3d6b4a96c86b9e76ad2ae9276f39ca
avasilevskaya <anst.vasilevskaya@gmail.com>
2022-05-23 23:23:45 +0300

    feat: copy with subfolders

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,15 +4,24 @@ const path = require('path');
 const pathCopyFrom = path.join(__dirname, 'files');
 const pathCopyTo = path.join(__dirname, 'files-copy');
 
-(async () => {
+async function copyDirectory(copyFrom, copyTo) {
   try {
-    await rm(pathCopyTo, { force: true, recursive: true });
-    await mkdir(pathCopyTo, { recursive: true });
-    const arrFiles = await readdir(pathCopyFrom, { withFileTypes: true });
+    const arrFiles = await readdir(copyFrom, { withFileTypes: true });
     for (let item of arrFiles) {
-      await copyFile(path.join(pathCopyFrom, item.name), path.join(pathCopyTo, item.name));
+      if (item.isFile()) {
+        await copyFile(path.join(copyFrom, item.name), path.join(copyTo, item.name));
+      } else if (item.isDirectory()) {
+        await mkdir(path.join(copyTo, item.name));
+        await copyDirectory(path.join(copyFrom, item.name), path.join(copyTo, item.name));
+      }
     }
   } catch (err) {
     console.log(`Ошибка: ${err.message}`);
   }
+}
+
+(async () => {
+  await rm(pathCopyTo, { force: true, recursive: true });
+  await mkdir(pathCopyTo, { recursive: true });
+  await copyDirectory(pathCopyFrom, pathCopyTo);
 })();
